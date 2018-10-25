36dbd9a12783f820d0fb5801ba675208efd9b2fc
mert1994 <ince@live.dk>
2018-10-25 14:02:06 +0200

    Created empty dataservice methods.

--- DataServiceLayer/Dataservice.cs
+++ DataServiceLayer/Dataservice.cs
@@ -16,11 +16,108 @@ namespace DataServiceLayer
             db = new StackOverflowContext();
         }
 
-        //Posts 
+        //Posts
         public List<Post> GetAllPosts()
         {
-            return db.Posts.OrderBy(x=> x.Id).ToList();
-
+            return db.Posts.OrderBy(x => x.Id).ToList();
         }
+
+        public List<Post> GetPostById(int id)
+        {
+            return null;
+        }
+
+        public Post GetAnswerById(int id)
+        {
+            return null;
+        }
+
+        //Comments
+        public List<Comment> GetAllComments()
+        {
+            return null;
+        }
+
+        public Comment GetCommentById(int id)
+        {
+            return null;
+        }
+
+        public Comment GetCommentByPostId(int id)
+        {
+            return null;
+        }
+
+        //Users
+        public User GetUser(int id)
+        {
+            return null;
+        }
+
+        public bool CreateUser(User user)
+        {
+            return false;
+        }
+
+        /*
+        public bool UpdateUser(User user)
+        {
+            return false;
+        }
+        */
+
+        //Marks
+        public Mark GetAllMarks()
+        {
+            return null;
+        }
+
+        public Mark GetMarkById(int id)
+        {
+            return null;
+        }
+
+        public bool CreateMark(int postId, int userId)
+        {
+            return false;
+        }
+
+        public bool DeleteMark(int postId, int userId)
+        {
+            return false;
+        }
+
+        //Searches
+        public Search GetSearchByString(string search)
+        {
+            return null;
+        }
+
+        public bool CreateSearchByString(int userId, string search)
+        {
+            return false;
+        }
+
+        public List<Search> GetSearchBySearchType(string searchtype)
+        {
+            return null;
+        }
+
+        public List<Search> GetAllSearches(string search)
+        {
+            return null;
+        }
+
+        public Search GetSearchById(int id)
+        {
+            return null;
+        }
+
+        //Author
+        public Author GetAuthor(int id)
+        {
+            return null;
+        }
+
     }
 }
