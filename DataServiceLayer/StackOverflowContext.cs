2f5e1f80c49ad92dedb6fcc58ed6ed90d28abfb1
mert1994 <ince@live.dk>
2018-10-25 10:29:42 +0200

    Started creating db context.

---
+++ DataServiceLayer/StackOverflowContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataServiceLayer
+{
+    public class StackOverflowContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql("host=localhost;db=stackoverflow;uid=postgres;pwd=mert1994;");
+        }
+    }
+}
