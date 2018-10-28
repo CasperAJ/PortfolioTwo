00dc4d037dc8c4ab69cc487618092aae949bc5a7
kay <kadiryorulmaz1@hotmail.com>
2018-10-28 10:18:03 +0100

    Comments controller fetch data from dataservicelayer

---
+++ PortfolioTwo/Controllers/CommentsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataServiceLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioTwo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+
+        public DataService _dataService;
+
+        public CommentsController(DataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllComments()
+        {
+            var comments = _dataService.GetAllComments();
+
+            return Ok(comments);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetCommentById(int id)
+        {
+            var comment = _dataService.GetCommentById(id);
+
+            return Ok(comment);
+        }
+
+        [HttpGet("postid/{id}")]
+        public IActionResult GetCommentsByPostId(int id)
+        {
+            var comment = _dataService.GetCommentsByPostId(id);
+
+            return Ok(comment);
+        }
+
+    }
+}
+
+
