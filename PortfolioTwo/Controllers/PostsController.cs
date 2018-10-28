dac4279e97e3ab3ed55239d9300238046ab925f0
Mikkel Pedersen <mikkelpedersen92@gmail.com>
2018-10-28 08:47:55 +0100

    added base of postscontroller.

---
+++ PortfolioTwo/Controllers/PostsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataServiceLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioTwo.Controllers
+{
+    // TODO: implements viewmodels here.
+    [Route("api/posts")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+
+        private DataService _dataservice;
+
+        public PostsController(DataService dataservice)
+        {
+            _dataservice = dataservice;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var posts = _dataservice.GetAllPosts();
+            return Ok(posts);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _dataservice.GetPostById(id);
+            if (post == null) return NotFound();
+
+            return Ok(post);
+        }
+
+
+        [HttpGet]
+        [Route("{id}/children")]
+        public IActionResult GetAnswers(int id)
+        {
+            var posts = _dataservice.GetAnswerById(id);
+            return Ok(posts);
+        }
+
+
+
+
+    }
+}
