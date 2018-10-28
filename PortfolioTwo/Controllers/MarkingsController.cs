95cebe42b151a700443a9ac1b733e5036fc3ddf1
Mikkel Pedersen <mikkelpedersen92@gmail.com>
2018-10-28 08:57:35 +0100

    added base for markings controller.

---
+++ PortfolioTwo/Controllers/MarkingsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataServiceLayer;
+using DataServiceLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioTwo.Controllers
+{
+    [Route("api/markings")]
+    [ApiController]
+    public class MarkingsController : ControllerBase
+    {
+        private DataService _dataservice;
+
+        public MarkingsController(DataService dataservice)
+        {
+            _dataservice = dataservice;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get(int id)
+        {
+            var markings = _dataservice.GetAllMarksByUser(id);
+            return Ok(markings);
+
+        }
+
+        // TODO: implement exception checking here.
+        [HttpPost]
+        public IActionResult Create(Mark mark)
+        {
+            var newmark = _dataservice.CreateMark(mark.PostId, mark.UserId, mark.Type, mark.Note);
+            return Created("", newmark);
+        }
+
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete(Mark mark)
+        {
+            var check = _dataservice.DeleteMark(mark.PostId, mark.UserId, mark.Type);
+            if (check) return Ok();
+            return NotFound();
+        }
+
+
+
+
+
+    }
+}
