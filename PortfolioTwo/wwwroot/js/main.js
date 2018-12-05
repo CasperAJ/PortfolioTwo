c2d05d80255858019439d7b5ad17f735b3cf7237
kay <kadiryorulmaz1@hotmail.com>
2018-12-05 21:01:24 +0100

    Template test

---
+++ PortfolioTwo/wwwroot/js/main.js
@@ -0,0 +1,12 @@
+﻿require.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: 'lib/jQuery/dist/jquery',
+        knockout: 'lib/knockout/dist/knockout'
+    }
+});
+
+
+require(['knockout', 'app/viewModel'], function (ko, vm) {
+    ko.applyBindings(vm);
+});
