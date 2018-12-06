77cd315fb6388113dd9e8e18dd42c34a46cb679e
kay <kadiryorulmaz1@hotmail.com>
2018-12-06 12:31:07 +0100

    Component udvikling

---
+++ PortfolioTwo/wwwroot/js/components/posts/postsList/postsList.js
@@ -0,0 +1,46 @@
+﻿define(['knockout', 'dataService'], function (ko, ds) {
+    return function (params) {
+        var posts = ko.observableArray();
+        var next = ko.observable("");
+        var prev = ko.observable("");
+        var getPostsApi = ko.observable("api/posts");
+
+        var clickNextPrev = function (link) {
+            getPostsApi(link);
+            callPosts();
+        };
+
+        function callPosts() {
+            ds.getPosts(getPostsApi(), function (data) {
+                console.log(data.data, "DATADATAD");
+                posts(data.data);
+
+                nextLink = data.paging.next;
+                prevLink = data.paging.prev;
+                if (nextLink !== null) {
+                    next(nextLink.replace("http://localhost:5000/", ""));
+                    console.log(next());
+                }
+                if (prevLink !== null) {
+                    console.log(prev());
+                    prev(prevLink.replace("http://localhost:5000/", ""));
+                }
+            });    
+        }
+
+        callPosts();
+        
+        //postman.subscribe("deletePerson", function (id) {
+        //    persons.remove(function (x) {
+        //        return x.id === id;
+        //    });
+        //});
+
+        return {
+            posts,
+            next,
+            prev,
+            clickNextPrev
+        };
+    };
+});
--- PortfolioTwo/wwwroot/js/main.js
+++ PortfolioTwo/wwwroot/js/main.js
@@ -1,11 +1,29 @@
-﻿require.config({
+﻿/// <reference path="services/dataservice.js" />
+require.config({
     baseUrl: 'js',
     paths: {
         jquery: 'lib/jQuery/dist/jquery',
-        knockout: 'lib/knockout/dist/knockout'
+        knockout: 'lib/knockout/dist/knockout',
+        text: 'lib/text/text',
+        dataService : 'services/dataService'
     }
 });
 
+// register components
+require(['knockout'], function (ko) {
+    ko.components.register("posts-list",
+        {
+            viewModel: { require: 'components/posts/postsList/postsList' },
+            template: { require: 'text!components/posts/postsList/postsListView.html' }
+        });
+
+    ko.components.register("post",
+        {
+            viewModel: { require: 'components/posts/posts/post' },
+            template: { require: 'text!components/posts/posts/postView.html' }
+        });
+
+});
 
 require(['knockout', 'app/viewModel'], function (ko, vm) {
     ko.applyBindings(vm);
