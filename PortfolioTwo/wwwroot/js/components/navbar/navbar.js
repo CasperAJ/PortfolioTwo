794ed00ca917281c13f1ead3e5804d1ecedea413
kay <kadiryorulmaz1@hotmail.com>
2018-12-11 23:58:23 +0100

    Added new js file

---
+++ PortfolioTwo/wwwroot/js/components/navbar/navbar.js
@@ -0,0 +1,20 @@
+﻿define(["jquery", "knockout", 'dataService', 'postman', 'app'], function ($, ko, ds, postman, vm) {
+    return function (params) {
+        var singlePost = ko.observable();
+
+
+        ds.getSinglePost(function (data) {
+            singlePost(data);
+        });
+
+        var changeDropdown = function () {
+            postman.publish("changedropdownSearch");
+        };
+
+
+        return {
+            singlePost,
+            changeDropdown
+        };
+    };
+});
